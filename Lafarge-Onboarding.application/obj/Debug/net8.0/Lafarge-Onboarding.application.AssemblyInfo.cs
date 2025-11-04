0c14da5d14598a5a9d4ad8ad9a14e2cd54489a0b
PATRICK-ADA <donpat646@gmail.com>
2025-11-04 14:28:59 +0100

    recent changes

--- Lafarge-Onboarding.application/obj/Debug/net8.0/Lafarge-Onboarding.application.AssemblyInfo.cs
+++ Lafarge-Onboarding.application/obj/Debug/net8.0/Lafarge-Onboarding.application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lafarge-Onboarding.application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7b054872dab1500b8a2228f1c3e1722204bc0648")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+990431d443078bed3ed7b7dc6930bfc80d8ba9ec")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lafarge-Onboarding.application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lafarge-Onboarding.application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
